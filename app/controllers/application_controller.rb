6ad39be38a0e2a90cf1b22faf2b9c7f9ccb472ba
mittall <mittalp.virtueinfo@gmail.com>
2015-08-06 18:14:00 +0530

    google maps master

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def address(string)
+    @hash[:marker_address] = string
+  end
+
+end
